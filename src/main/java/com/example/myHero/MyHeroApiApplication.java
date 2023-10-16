bda6871900d38a17b8cfc7d7783c3da55848e84f
Charlos16v <carluitosgfx@gmail.com>
2023-10-16 12:51:10 +0200

    build: Spring Boot skeleton

---
+++ src/main/java/com/example/myHero/MyHeroApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.myHero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHeroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHeroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myHero/MyHeroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myHero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHeroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
