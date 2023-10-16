ceb4aa3ea03ed115cca5bf857e35b70247fed2fb
Charlos16v <carluitosgfx@gmail.com>
2023-10-16 15:04:33 +0200

    test: added junit 5 dep and unit testing for Hero class

---
+++ src/test/java/com/example/myHero/hero/HeroTest.java
@@ -0,0 +1,55 @@
+package com.example.myHero.hero;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HeroTest {
+
+    private Hero hero;
+
+    @BeforeEach
+    void setUp() {
+        hero = new Hero(1, "Aragorn", "The King of Gondor", LocalDate.of(1970, Month.JANUARY, 1));
+    }
+
+    @Test
+    void testConstructorWithId() {
+        assertEquals(1, hero.getId());
+        assertEquals("Aragorn", hero.getName());
+        assertEquals("The King of Gondor", hero.getDescription());
+        assertEquals(LocalDate.of(1970, Month.JANUARY, 1), hero.getDob());
+    }
+
+    @Test
+    void testConstructorWithoutId() {
+        Hero heroWithoutId = new Hero("Legolas", "Elven Archer", LocalDate.of(2931, Month.AUGUST, 24));
+        assertEquals("Legolas", heroWithoutId.getName());
+        assertEquals("Elven Archer", heroWithoutId.getDescription());
+        assertEquals(LocalDate.of(2931, Month.AUGUST, 24), heroWithoutId.getDob());
+    }
+
+    @Test
+    void testGetAge() {
+        int expectedAge = LocalDate.now().getYear() - LocalDate.of(1970, Month.JANUARY, 1).getYear();
+        assertEquals(expectedAge, hero.getAge());
+    }
+
+    @Test
+    void testSetters() {
+        hero.setId(2);
+        hero.setName("Gimli");
+        hero.setDescription("Dwarf Warrior");
+        hero.setDob(LocalDate.of(2879, Month.NOVEMBER, 21));
+
+        assertEquals(2, hero.getId());
+        assertEquals("Gimli", hero.getName());
+        assertEquals("Dwarf Warrior", hero.getDescription());
+        assertEquals(LocalDate.of(2879, Month.NOVEMBER, 21), hero.getDob());
+    }
+}
