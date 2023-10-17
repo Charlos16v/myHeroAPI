e62d52538efa38c61d7ddecd2c6bcb203bdc2b5a
Charlos16v <carluitosgfx@gmail.com>
2023-10-17 13:41:11 +0200

    build: implemented swagger with springdoc

--- src/main/java/com/example/myHero/MyHeroApiApplication.java
+++ src/main/java/com/example/myHero/MyHeroApiApplication.java
@@ -1,7 +1,10 @@
 package com.example.myHero;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyHeroApiApplication {
@@ -10,4 +13,13 @@ public class MyHeroApiApplication {
 		SpringApplication.run(MyHeroApiApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(
+				new Info()
+						.title("MyHeroAPI")
+						.version("1.0")
+						.description("Challenge for W2M.")
+		);
+	}
 }
