4297eb3626ff55a93e461fba1c0c50f31f30b9b1
Charlos16v <carluitosgfx@gmail.com>
2023-10-17 17:37:07 +0200

    test: created test for get hero by id

--- src/main/java/com/example/myHero/hero/HeroService.java
+++ src/main/java/com/example/myHero/hero/HeroService.java
@@ -1,8 +1,11 @@
 package com.example.myHero.hero;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HeroService {
     List<Hero> getAll();
+
+    Optional<Hero> findById(Long id);
 }
 
