eb50874cef1e76bf0cb657f52c31778826349b06
Charlos16v <carluitosgfx@gmail.com>
2023-10-17 17:24:31 +0200

    test: removed mocks and tested with real data

--- src/test/java/com/example/myHero/hero/HeroControllerTest.java
+++ src/test/java/com/example/myHero/hero/HeroControllerTest.java
@@ -2,65 +2,37 @@ package com.example.myHero.hero;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HeroControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @Mock
-    private HeroService heroService;
-
-    @InjectMocks
-    private HeroController heroController;
+    @Autowired
+    private HeroRepository heroRepository;
 
     @BeforeEach
     public void setup() {
-        MockitoAnnotations.openMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(heroController).build();
     }
 
     @Test
     public void testGetAllHeroes() throws Exception {
-        List<Hero> defaultHeroes = Arrays.asList(
-                new Hero(1L, "Superman", "Man of Steel", LocalDate.parse("1980-01-01")),
-                new Hero(2L, "Batman", "Dark Knight", LocalDate.parse("1975-03-15")),
-                new Hero(3L, "Wonder Woman", "Amazon Princess", LocalDate.parse("1985-06-10")),
-                new Hero(4L, "The Flash", "Speedster", LocalDate.parse("1990-12-05")),
-                new Hero(5L, "Aquaman", "King of Atlantis", LocalDate.parse("1988-08-20"))
-        );
-
-
-        when(heroService.getAll()).thenReturn(defaultHeroes);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
-        String heroesJson = objectMapper.writeValueAsString(defaultHeroes);
 
         mockMvc.perform(get("/hero")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(heroesJson));
+                .andExpect(content().json("[{\"id\":1,\"name\":\"Superman\",\"description\":\"Man of Steel\",\"dob\":\"1980-01-01\",\"age\":43},{\"id\":2,\"name\":\"Batman\",\"description\":\"Dark Knight\",\"dob\":\"1975-03-15\",\"age\":48},{\"id\":3,\"name\":\"Wonder Woman\",\"description\":\"Amazon Princess\",\"dob\":\"1985-06-10\",\"age\":38},{\"id\":4,\"name\":\"The Flash\",\"description\":\"Speedster\",\"dob\":\"1990-12-05\",\"age\":32},{\"id\":5,\"name\":\"Aquaman\",\"description\":\"King of Atlantis\",\"dob\":\"1988-08-20\",\"age\":35}]"));
     }
 }
-
-
