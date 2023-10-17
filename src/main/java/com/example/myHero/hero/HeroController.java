b71799964f1efa2966ec32174c77c03490e5c7dd
Charlos16v <carluitosgfx@gmail.com>
2023-10-17 17:48:04 +0200

    docs: added more documentation to the endpoints

--- src/main/java/com/example/myHero/hero/HeroController.java
+++ src/main/java/com/example/myHero/hero/HeroController.java
@@ -1,5 +1,10 @@
 package com.example.myHero.hero;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +24,17 @@ public class HeroController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all heroes", description = "Returns a list of all heroes.")
+    @ApiResponse(responseCode = "200", description = "List of heroes retrieved successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Hero.class)))
     public ResponseEntity<List<Hero>> getAll() {
         List<Hero> heroes = _heroService.getAll();
         return ResponseEntity.ok(heroes);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a hero by ID", description = "Returns a hero based on the provided ID.")
+    @ApiResponse(responseCode = "200", description = "Hero retrieved successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Hero.class)))
+    @ApiResponse(responseCode = "404", description = "Hero not found")
     public ResponseEntity<Hero> getById(@PathVariable Long id) {
         Optional<Hero> hero = _heroService.findById(id);
 
