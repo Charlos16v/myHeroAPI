df99c2edf01f0187039382472327505bffb6f4fc
Charlos16v <carluitosgfx@gmail.com>
2023-10-17 21:18:47 +0200

    feat: implemented simple cache to the methods in the service layer

--- src/main/java/com/example/myHero/MyHeroApiApplication.java
+++ src/main/java/com/example/myHero/MyHeroApiApplication.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class MyHeroApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/myHero/hero/HeroService.java
+++ src/main/java/com/example/myHero/hero/HeroService.java
@@ -1,18 +1,27 @@
 package com.example.myHero.hero;
 
+import org.springframework.cache.annotation.Cacheable;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface HeroService {
+    @Cacheable("heroCache")
     List<Hero> getAll();
 
+    @Cacheable("heroCache")
     Optional<Hero> findById(Long id);
+
+    @Cacheable("heroCache")
     List<Hero> searchHeroesByName(String searchParameter);
 
+    @Cacheable("heroCache")
     Hero createHero(Hero hero);
 
+    @Cacheable("heroCache")
     Hero modifyHero(Long id, Hero updatedHero);
 
+    @Cacheable("heroCache")
     void deleteHeroById(Long id);
 }
 
