628e74db617710167c818b3c84f3b0ec41128f03
Charlos16v <carluitosgfx@gmail.com>
2023-10-17 18:02:11 +0200

    feat: implemented the findByNameContaining method and enabled the IGNORECASE for case insensitive

--- src/main/java/com/example/myHero/hero/HeroRepository.java
+++ src/main/java/com/example/myHero/hero/HeroRepository.java
@@ -3,6 +3,9 @@ package com.example.myHero.hero;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HeroRepository extends JpaRepository<Hero, Long>{
+    List<Hero> findByNameContainingIgnoreCase(String searchParameter);
 }
